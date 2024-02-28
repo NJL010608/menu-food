9b89f820dc3ef89d069b13c323acc9e6f42c28a8
Jia Ler <jialerngwee@gmail.com>
2024-02-28 16:26:42 +0800

    Create css file and Update menu scss file, Content.js file

--- src/Content.js
+++ src/Content.js
@@ -4,23 +4,23 @@ import './css/menu.scss';
 function Content() {
     return (
     <div class="container">
-        <div class="section section--1">
-            <div class="section--1__titlebox">
-                <h3 class="section--1__subtitle">Chicken</h3>
+        <div class="section">
+            <div class="section__titlebox">
+                <h3 class="section__subtitle--chicken">Chicken</h3>
             </div>
           <p>Food, substance consisting essentially of protein, carbohydrate, fat, and other nutrients used in the body of an organism to sustain growth and vital processes and to furnish energy. The absorption and utilization of food by the body is fundamental to nutrition and is facilitated by digestion.</p>
         </div>
 
-        <div class="section section--2">
-        <div class="section--2__titlebox">
-          <h3 class="section--2__subtitle">Beef</h3>
+        <div class="section">
+        <div class="section__titlebox">
+          <h3 class="section__subtitle--beef">Beef</h3>
         </div>
           <p>Food, substance consisting essentially of protein, carbohydrate, fat, and other nutrients used in the body of an organism to sustain growth and vital processes and to furnish energy. The absorption and utilization of food by the body is fundamental to nutrition and is facilitated by digestion.</p>
         </div>
 
-        <div class="section section--3">
-        <div class="section--3__titlebox">
-          <h3 class="section--3__subtitle">Sushi</h3>
+        <div class="section">
+        <div class="section__titlebox">
+          <h3 class="section__subtitle--sushi">Sushi</h3>
         </div>
           <p>Food, substance consisting essentially of protein, carbohydrate, fat, and other nutrients used in the body of an organism to sustain growth and vital processes and to furnish energy. The absorption and utilization of food by the body is fundamental to nutrition and is facilitated by digestion.</p>
         </div>
