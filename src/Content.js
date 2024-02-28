b2ac22b4bf2cbbb39992a2aa78efb44db243677e
Jia Ler <jialerngwee@gmail.com>
2024-02-28 14:53:08 +0800

    Update menu scss file and Content.js file

--- src/Content.js
+++ src/Content.js
@@ -4,47 +4,27 @@ import './css/menu.scss';
 function Content() {
     return (
     <div class="container">
-        <div class="section">
-            <div class="section__titlebox">
-                <h3 class="section__subtitle">Chicken</h3>
+        <div class="section section--1">
+            <div class="section--1__titlebox">
+                <h3 class="section--1__subtitle">Chicken</h3>
             </div>
-          <p>food, substance consisting essentially of protein, carbohydrate, fat, and other nutrients used in the body of an organism to sustain growth and vital processes and to furnish energy. The absorption and utilization of food by the body is fundamental to nutrition and is facilitated by digestion.</p>
+          <p>Food, substance consisting essentially of protein, carbohydrate, fat, and other nutrients used in the body of an organism to sustain growth and vital processes and to furnish energy. The absorption and utilization of food by the body is fundamental to nutrition and is facilitated by digestion.</p>
         </div>
 
-        <div class="section">
-        <div class="section__titlebox">
-          <h3 class="section__subtitle2">Beef</h3>
+        <div class="section section--2">
+        <div class="section--2__titlebox">
+          <h3 class="section--2__subtitle">Beef</h3>
         </div>
-          <p>food, substance consisting essentially of protein, carbohydrate, fat, and other nutrients used in the body of an organism to sustain growth and vital processes and to furnish energy. The absorption and utilization of food by the body is fundamental to nutrition and is facilitated by digestion.</p>
-        </div>
-
-        <div class="section--tablet1">
-            <div class="section__titlebox">
-                <h3 class="section__subtitle">Chicken</h3>
-            </div>
-          <p>food, substance consisting essentially of protein, carbohydrate, fat, and other nutrients used in the body of an organism to sustain growth and vital processes and to furnish energy. The absorption and utilization of food by the body is fundamental to nutrition and is facilitated by digestion.</p>
+          <p>Food, substance consisting essentially of protein, carbohydrate, fat, and other nutrients used in the body of an organism to sustain growth and vital processes and to furnish energy. The absorption and utilization of food by the body is fundamental to nutrition and is facilitated by digestion.</p>
         </div>
 
-        <div class="section--tablet1">
-        <div class="section__titlebox">
-          <h3 class="section__subtitle2">Beef</h3>
+        <div class="section section--3">
+        <div class="section--3__titlebox">
+          <h3 class="section--3__subtitle">Sushi</h3>
         </div>
-          <p>food, substance consisting essentially of protein, carbohydrate, fat, and other nutrients used in the body of an organism to sustain growth and vital processes and to furnish energy. The absorption and utilization of food by the body is fundamental to nutrition and is facilitated by digestion.</p>
-        </div>
-
-        <div class="section">
-        <div class="section__titlebox">
-          <h3 class="section__subtitle3">Sushi</h3>
-        </div>
-          <p>food, substance consisting essentially of protein, carbohydrate, fat, and other nutrients used in the body of an organism to sustain growth and vital processes and to furnish energy. The absorption and utilization of food by the body is fundamental to nutrition and is facilitated by digestion.</p>
+          <p>Food, substance consisting essentially of protein, carbohydrate, fat, and other nutrients used in the body of an organism to sustain growth and vital processes and to furnish energy. The absorption and utilization of food by the body is fundamental to nutrition and is facilitated by digestion.</p>
         </div>
 
-        <div class="section--tablet2">
-        <div class="section__titlebox">
-          <h3 class="section__subtitle3">Sushi</h3>
-        </div>
-          <p>food, substance consisting essentially of protein, carbohydrate, fat, and other nutrients used in the body of an organism to sustain growth and vital processes and to furnish energy. The absorption and utilization of food by the body is fundamental to nutrition and is facilitated by digestion.</p>
-        </div>
     </div>
     );
 }
