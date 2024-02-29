f394185b421037571680ee68f58c40f3f152764f
Jia Ler <jialerngwee@gmail.com>
2024-02-29 12:13:12 +0800

    Update css, scss, content file & Create webpack

--- src/Content.js
+++ src/Content.js
@@ -3,24 +3,24 @@ import './css/menu.scss';
 
 function Content() {
     return (
-    <div class="container">
-        <div class="section">
-            <div class="section__titlebox">
-                <h3 class="section__subtitle--chicken">Chicken</h3>
+    <div className="container">
+        <div className="section">
+            <div className="section__titlebox">
+                <h3 className="section__subtitle section__subtitle--chicken">Chicken</h3>
             </div>
           <p>Food, substance consisting essentially of protein, carbohydrate, fat, and other nutrients used in the body of an organism to sustain growth and vital processes and to furnish energy. The absorption and utilization of food by the body is fundamental to nutrition and is facilitated by digestion.</p>
         </div>
 
-        <div class="section">
-        <div class="section__titlebox">
-          <h3 class="section__subtitle--beef">Beef</h3>
+        <div className="section">
+        <div className="section__titlebox">
+          <h3 className="section__subtitle section__subtitle--beef">Beef</h3>
         </div>
           <p>Food, substance consisting essentially of protein, carbohydrate, fat, and other nutrients used in the body of an organism to sustain growth and vital processes and to furnish energy. The absorption and utilization of food by the body is fundamental to nutrition and is facilitated by digestion.</p>
         </div>
 
-        <div class="section">
-        <div class="section__titlebox">
-          <h3 class="section__subtitle--sushi">Sushi</h3>
+        <div className="section">
+        <div className="section__titlebox">
+          <h3 className="section__subtitle section__subtitle--sushi">Sushi</h3>
         </div>
           <p>Food, substance consisting essentially of protein, carbohydrate, fat, and other nutrients used in the body of an organism to sustain growth and vital processes and to furnish energy. The absorption and utilization of food by the body is fundamental to nutrition and is facilitated by digestion.</p>
         </div>
